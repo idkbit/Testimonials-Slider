ad24fcc4af8d09f619288667b17e7871a38cc87a
Valikhan <ggwpanda@gmail.com>
2021-05-27 01:31:13 +0600

    remake

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-import people from "./data.js"
-console.log(people)
+
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
-const photoContainer = document.querySelector(".photo-container");
-const textContainer = document.querySelector(".text-container");
-const nameContainer = document.querySelector(".name-container");
-const jobContainer = document.querySelector(".job-container");
 
-let counter = 0;
+
 
 nextBtn.addEventListener("click", () => {
 
