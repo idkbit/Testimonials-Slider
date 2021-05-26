5f24ac00c37ae1c3d12e333cc7c66bef6cb0f8c6
Valikhan <ggwpanda@gmail.com>
2021-05-27 00:13:41 +0600

    finished mobile look

---
+++ app.js
@@ -0,0 +1,8 @@
+import people from "./data.js"
+console.log(people)
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+nextBtn.addEventListener("click", () => {
+
+});
---
+++ data.js
@@ -0,0 +1,22 @@
+const people = [
+  {
+    photo: "./images/image-tanya.jpg",
+    text: `“ I’ve been interested in coding for a while but never taken the jump, until now.
+            I couldn’t recommend this course enough. I’m now in the job of my dreams and so
+            excited about the future. ”`,
+    name: "Tanya Sinclair",
+    job: "UX Engineer",
+
+  },
+  {
+    photo: "./images/image-john.jpg",
+    text: `“ If you want to lay the best foundation possible I’d recommend taking this course.
+            The depth the instructors go into is incredible. I now feel so confident about
+            starting up as a professional developer. ”`,
+    name: "John Tarkpor",
+    job: "Junior Front-end Developer",
+
+  }
+]
+
+export default people;
