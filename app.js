4874721ae0374bc6c6c828f3179c68c1ca561119
Valikhan <ggwpanda@gmail.com>
2021-05-27 01:04:00 +0600

    .

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ import people from "./data.js"
 console.log(people)
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
+const photoContainer = document.querySelector(".photo-container");
+const textContainer = document.querySelector(".text-container");
+const nameContainer = document.querySelector(".name-container");
+const jobContainer = document.querySelector(".job-container");
+
+let counter = 0;
 
 nextBtn.addEventListener("click", () => {
+
+});
+
+prevBtn.addEventListener("click", () => {
 
 });
